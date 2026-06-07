04a81da042d0a4cb179411ee15f1514253624bb8
Shadowlord31 <shadowlord31@users.noreply.github.com>
2026-06-07 22:35:57 +0200

    feat: initial project structure with manifest and constants

---
+++ custom_components/smart_heating_advisor/const.py
@@ -0,0 +1,15 @@
+DOMAIN = "smart_heating_advisor"
+
+CONF_OUTDOOR_TEMP = "outdoor_temp_sensor"
+CONF_INDOOR_TEMPS = "indoor_temp_sensors"
+CONF_WEATHER_ENTITY = "weather_entity"
+CONF_WINDOW_SENSORS = "window_sensors"
+CONF_HEATING_THRESHOLD = "heating_threshold"
+CONF_SUMMER_MODE_DAYS = "summer_mode_days"
+CONF_MIN_INDOOR_TEMP = "min_indoor_temp"
+
+DEFAULT_HEATING_THRESHOLD = 15.0
+DEFAULT_SUMMER_MODE_DAYS = 3
+DEFAULT_MIN_INDOOR_TEMP = 20.0
+
+UPDATE_INTERVAL_MINUTES = 30
